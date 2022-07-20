66029a03ec57591901d781d45dd392305dc4de0a
Yazzyk <917885215@qq.com>
2022-07-20 15:23:36 +0800

    add example params

--- services/examplesvs/slot.go
+++ services/examplesvs/slot.go
@@ -1,15 +1,17 @@
 package examplesvs
 
 import (
+	"fmt"
 	"github.com/drharryhe/has/common/hlogger"
 	"github.com/drharryhe/has/core"
 )
 
 type ExampleRequest struct {
 	core.SlotRequestBase
+	Name string `json:"name" data:"required"`
 }
 
 func (this *Service) ExampleSlot(req *ExampleRequest, res *core.SlotResponse) {
-	hlogger.Info(req)
-	this.Response(res, req,nil)
+	hlogger.Info(req.Name)
+	this.Response(res, fmt.Sprintf("Hello %s", req.Name),nil)
 }
